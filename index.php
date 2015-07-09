653e4d8d0ac1e50773e691e4bfc890f28110f1f1
Ali Parsai <ali.parsai@email.com>
2015-07-09 18:35:25 +0200

    initial

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+//include 'log.php';
+//log_ip();
+
+//echo "<meta http-equiv='refresh' content='0;url=http://www.parsai.net/blog'>";
+
+header('Location: http://www.parsai.net/research');
+
+?>
